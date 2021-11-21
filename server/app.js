8174026c1dbd47ae2e316f01e4dfa12251a3c5c8
pramit-marattha <pramitmarattha5497@gmail.com>
2021-11-21 13:59:41 +0545

    server all configured

--- server/app.js
+++ server/app.js
@@ -5,30 +5,31 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
+// app config
 app.use(cors());
 app.use(express.json());
 
-// DB connection
-const DATABASE_URL = process.env.MONGO_DB_URL;
-mongoose.connect(DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// port and DB config
+const DATABASE_CONNECTION = process.env.DATABASE_URL;
+const PORT = process.env.PORT || 5000;
 
 // mongoose connection
-mongoose.connection.once("open", () => {
-  console.log("Mongo Database successfully connected !!");
-});
+mongoose
+  .connect(DATABASE_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server is running at : http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.error(error));
 
 // routers
-const mealsRouter = require("./routes/meals");
-const usersRouter = require("./routes/users");
+const calorie = require("./routes/calorie.routes.js");
+const users = require("./routes/users.routes.js");
 
-app.use("/meals", mealsRouter);
-app.use("/users", usersRouter);
-
-app.listen(port, () => {
-  console.log(`Server is currently running at: http://localhost:${port}`);
-});
+app.use("/calorie", calorie);
+app.use("/users", users);
